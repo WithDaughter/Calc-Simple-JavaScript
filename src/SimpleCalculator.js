09a81fffe307156de6a842dbc2938c7ee0228c99
John Park <Rtos9er@gmail.com>
2025-05-19 22:47:30 +0900

    calculate

--- src/SimpleCalculator.js
+++ src/SimpleCalculator.js
@@ -1,3 +1,13 @@
-const main = _ => 'Hello'
+const print = console.log
+const calculate = src => {
+    return src
+}
+////////////////////////
+export default calculate
 
-export default main
+;(() => {
+    let src = '1+2'
+    // src = '1+2+3'
+    const val = calculate(src)
+    print(val)
+})()
