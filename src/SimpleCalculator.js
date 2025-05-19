9b3305e4b41dd6a4e18a1f0313e8aa88ec2a89ee
John Park <Rtos9er@gmail.com>
2025-05-20 02:07:37 +0900

    공백 제거, 1.23 가능

--- src/SimpleCalculator.js
+++ src/SimpleCalculator.js
@@ -2,11 +2,11 @@ const print = console.log
 
 const divide = exp => {
     if (exp.includes('/')) {
-        const tokens = exp.split('/')
+        const tokens = exp.split('/').map(t => t.trim())
         const token = tokens.shift()
         return tokens.map(Number).reduce((acc, cur) => acc / cur, token)
     } else
-        return Number(exp)
+        return Number(exp.trim())
 }
 
 const multiply = exp => {
@@ -19,7 +19,7 @@ const multiply = exp => {
 const minus = exp => {
     if (exp.includes('-')) {
         const tokens = exp.split('-')
-        const token = tokens.shift()
+        const token = tokens.shift().trim()
         return tokens.map(multiply).reduce((acc, cur) => acc - cur, token)
     } else
         return multiply(exp)
@@ -33,12 +33,12 @@ const parenthesis = src => {
     const mixSign = src => src.replace(/\+-/, '-').replace(/--/, '+')
     if (src.includes('(')) {
         const start = src.indexOf('(')
-        const val = parenthesis(src.slice(start + 1))
-        return parenthesis(mixSign(src.slice(0, start) + val))
+        const val = parenthesis(src.slice(start + 1).trim())
+        return parenthesis(mixSign(src.slice(0, start).trim() + val))
     } else if (src.includes(')')) {
         const end = src.indexOf(')')
         const val = plus(src.slice(0, end))
-        return mixSign(String(val) + src.slice(end + 1))
+        return mixSign(String(val) + src.slice(end + 1).trim())
     } else
         return src
 }
@@ -52,7 +52,8 @@ const calculate = src => {
 export default calculate
 
 ;(() => {
-    const src = '2-(1-5)'
+    // const src = '2 - (1 - 5)'
+    const src = '( 100 - (100 - 200) ) * 30'
     const val = calculate(src)
     print(val)
 })()
