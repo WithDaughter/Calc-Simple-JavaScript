3a0fbbd138dfc6df0444355376dd4c854bf25ef6
John Park <Rtos9er@gmail.com>
2025-05-20 02:56:43 +0900

    test

--- src/SimpleCalculator.js
+++ src/SimpleCalculator.js
@@ -3,7 +3,7 @@ const print = console.log
 const divide = exp => {
     if (exp.includes('/')) {
         const tokens = exp.split('/').map(t => t.trim())
-        const token = tokens.shift()
+        const token = Number(tokens.shift())
         return tokens.map(Number).reduce((acc, cur) => acc / cur, token)
     } else
         return Number(exp.trim())
@@ -19,7 +19,7 @@ const multiply = exp => {
 const minus = exp => {
     if (exp.includes('-')) {
         const tokens = exp.split('-')
-        const token = tokens.shift().trim()
+        const token = multiply(tokens.shift().trim())
         return tokens.map(multiply).reduce((acc, cur) => acc - cur, token)
     } else
         return multiply(exp)
@@ -51,9 +51,36 @@ const calculate = src => {
 ////////////////////////
 export default calculate
 
-;(() => {
-    // const src = '2 - (1 - 5)'
-    const src = '( 100 - (100 - 200) ) * 30'
+const assert = (src, expected) => {
     const val = calculate(src)
-    print(val)
+    if (val === expected) {
+        print(`성공: ${src} == ${expected}`)
+    } else
+        print(`실패: ${src} => ${val} != ${expected}`)
+}
+
+const tests = [
+    {code: '1 + 2', expected: 3},
+    {code: '1 + 2 + 3', expected: 6},
+    {code: '2 * 3', expected: 6},
+    {code: '1 + 2 * 3', expected: 7},
+    {code: '2 * 3 + 3', expected: 9},
+    {code: '1 - 3 - 3', expected: -5},
+    {code: '1 - 3 + 5', expected: 3},
+    {code: '1 + 3 - 3', expected: 1},
+    {code: '8 / 2 - 1', expected: 3},
+    {code: '9 / 3 - 2', expected: 1},
+    {code: '(1 + 2) * 3', expected: 9},
+    {code: '2 - (1 - 5)', expected: 6},
+    {code: '( 100 - (100 - 200) ) * 30', expected: 6000},
+    {code: '4 *(1 + ((2 * 5) /  (2 + 3))) * 3', expected: 36},
+    {code: '((10 - (3 *3)) + 2) * ((10 / 5) +2)', expected: 12},
+]
+
+;(() => {
+    try {
+        tests.forEach(test => assert(test.code, test.expected))
+    } catch (e) {
+        print(e)
+    }
 })()
